60414126fc213a7cc27899d5cbd6ffb3d83bcd30
ironcg20 <149658174+ironcg20@users.noreply.github.com>
2023-12-01 12:28:18 -0600

    23-12-01-1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShowTodoList from "./components/showTodoList";
 import CreateTodo from "./components/createTodo";
@@ -11,6 +11,34 @@ import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 
 function App() {
+  // const [loggedIn, setLoggedIn] = useState(false);
+  // const [data, setData] = useState({ email: "", password: "" });
+
+  const handleSignIn = async () => {
+    try {
+      const user = await SignIn("email", "password"); // Call the signIn function with actual credentials
+      handleLoginSuccess(user.email); // Assuming you have a function to handle successful login
+    } catch (error) {
+      console.error("Sign-in failed", error);
+      // Handle sign-in error (e.g., display error message)
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      const user = await SignUp("email", "password"); // Call the signUp function with actual credentials
+      handleLoginSuccess(user.email); // Assuming you have a function to handle successful login
+    } catch (error) {
+      console.error("Sign-up failed", error);
+      // Handle sign-up error (e.g., display error message)
+    }
+  };
+
+  const handleLoginSuccess = (email) => {
+    // Update state or perform actions upon successful login
+    console.log("Logged in as:", email);
+  };
+
   return (
     <div className='app-contents'>
       <Router>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
@@ -13,8 +13,45 @@ const style = {
   flexGrow: 1,
 };
 const NavBar = ({ handleSignIn, handleSignUp }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState("");
+
+  const handleSignOut = () => {
+    // Handle sign out logic here
+    setLoggedIn(false);
+    setUserName("");
+  };
+
+  const handleLoginSuccess = (userName) => {
+    // Handle successful login logic here, set the user name and login status
+    setLoggedIn(true);
+    setUserName(userName);
+  };
   // const [user, setUser] = useState("");
 
+  // return (
+  //   <div>
+  //     <AppBar position='static'>
+  //       <Toolbar>
+  //         <IconButton edge='start' color='inherit' aria-label='Menu'>
+  //           <MenuIcon />
+  //         </IconButton>
+  //         <Typography variant='h6' style={style}>
+  //           Mern Stack Todo List
+  //         </Typography>
+  //         {/* <Route path='/signup'  /> */}
+
+  //         <p name='userName' id='userName'></p>
+  //         <Link to={`/signin/`}>
+  //           <Button onClick={handleSignIn}>Sign in</Button>
+  //         </Link>
+  //         <Link to={`/signup/`}>
+  //           <Button onClick={handleSignUp}>Sign Up</Button>
+  //         </Link>
+  //       </Toolbar>
+  //     </AppBar>
+  //   </div>
+  // );
   return (
     <div>
       <AppBar position='static'>
@@ -25,15 +62,23 @@ const NavBar = ({ handleSignIn, handleSignUp }) => {
           <Typography variant='h6' style={style}>
             Mern Stack Todo List
           </Typography>
-          {/* <Route path='/signup'  /> */}
-
-          <p name='userName' id='userName'></p>
-          <Link to={`/signin/`}>
-            <Button onClick={handleSignIn}>Sign in</Button>
-          </Link>
-          <Link to={`/signup/`}>
-            <Button onClick={handleSignUp}>Sign Up</Button>
-          </Link>
+          {loggedIn && (
+            <p name='userName' id='userName'>
+              {userName}
+            </p>
+          )}
+          {loggedIn ? (
+            <Button onClick={handleSignOut}>Sign out</Button>
+          ) : (
+            <>
+              <Link to={`/signin/`}>
+                <Button onClick={handleSignIn}>Sign in</Button>
+              </Link>
+              <Link to={`/signup/`}>
+                <Button onClick={handleSignUp}>Sign Up</Button>
+              </Link>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </div>
